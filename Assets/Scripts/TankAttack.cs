26d915bc365796e34335c0b9b1c0d85b6c2a54e7
Deviltxx <adeviltxx@163.com>
2020-02-21 14:31:35 +0800

    tank

---
+++ Assets/Scripts/TankAttack.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankAttack : MonoBehaviour {
+
+    public GameObject ShellPrefab;
+    public KeyCode fireKey = KeyCode.Space;
+    public float shellSpeed = 20;
+	public AudioClip shotAudio;
+
+    private Transform firePosition;
+
+
+
+	// Use this for initialization
+	void Start () {
+	    firePosition = transform.Find("firePosition");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(fireKey)) 
+		{
+			AudioSource.PlayClipAtPoint(shotAudio,transform.position);
+	        GameObject go = GameObject.Instantiate(ShellPrefab, firePosition.position, firePosition.rotation) as GameObject;
+	        go.GetComponent<Rigidbody>().velocity = go.transform.forward*shellSpeed;
+
+	    }
+	}
+}
+
